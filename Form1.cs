10cc3c84fe5605140a9e49decabc00c134848a8a
Ryan Graue <grauer@spu.edu>
2021-03-07 21:05:12 -0800

    final additions

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+ * Author:          Ryan Graue
+ * Date:            March 7th, 2021
+ * Title:           Project 06
+ * Desc:            App to record and store ice cream orders. recors are stored in local sql database (Project06).
+ */
+
+using System;
 using System.Data.SqlClient;
 using System.Windows.Forms;
 
@@ -16,7 +23,7 @@ namespace Project_06
 
         private decimal totalPrice = 0.00M;
 
-        private string stringConnect = "data source = (local);DataBase=Project06; Integrated Security = SSPI";
+        private string stringConnect = "data source = (local);DataBase=Project06; Integrated Security = SSPI";      // DATABASE = Project06
         private SqlConnection conn = new SqlConnection();
         
 
@@ -24,7 +31,11 @@ namespace Project_06
         {
             InitializeComponent();
         }
-
+        /// <summary>
+        /// Adds the total price of the specific cone to the order
+        /// </summary>
+        /// <param name="sender">button object which triggers the event</param>
+        /// <param name="e">arguments included with the event</param>
         private void buttonAdd_Click(object sender, EventArgs e)
         {
             decimal orderPrice = 0.00M;
@@ -58,13 +69,22 @@ namespace Project_06
             listBoxOrders.Items.Add(orderPrice);
             totalPrice += orderPrice;
         }
-
+        /// <summary>
+        /// Clears the current order
+        /// </summary>
+        /// <param name="sender">button object which triggers the event</param>
+        /// <param name="e">arguments included with the event</param>
         private void buttonClear_Click(object sender, EventArgs e)
         {
             totalPrice = 0.0M;
             listBoxOrders.Items.Clear();
         }
 
+        /// <summary>
+        /// Sums the total order. Sends ther order total to to the local sql database
+        /// </summary>
+        /// <param name="sender">button object which triggers the event</param>
+        /// <param name="e">arguments included with the event</param>
         private void buttonTotal_Click(object sender, EventArgs e)
         {
             string date = DateTime.Today.ToString("MM/dd/yyyy");
@@ -83,6 +103,11 @@ namespace Project_06
             conn.Close();
         }
 
+        /// <summary>
+        /// Adds up the total transactions for the day
+        /// </summary>
+        /// <param name="sender">button object which triggers the event</param>
+        /// <param name="e">arguments included with the event</param>
         private void buttonTotalTrans_Click(object sender, EventArgs e)
         {
 
@@ -95,17 +120,21 @@ namespace Project_06
             textBoxTransSummary.Text = "$" + dayTotal;
         }
 
+        /// <summary>
+        /// returns a unique primary key int for each entity in the database. Recursivley checks to find unique int
+        /// </summary>
+        /// <returns>unique int for primary key</returns>
         private int getUniqueId ()
         {
             conn.ConnectionString = stringConnect;
             conn.Open();
             Random rand = new Random();
             int temp = rand.Next(100001);
-            SqlCommand search = new SqlCommand("Select Id From Orders Where Id =" + temp, conn);
+            SqlCommand search = new SqlCommand("Select Id From Orders Where Id =" + temp, conn);        // sql query for id num
             SqlDataReader reader = search.ExecuteReader();
             if (reader.HasRows)
             {
-                return getUniqueId();
+                return getUniqueId();     
             } else
             {
                 conn.Close();
